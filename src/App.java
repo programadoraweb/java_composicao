d8880ee7cffc84c90eb2c249e0c9549666a5544e
pw <programadoraweb3.0@gmail.com>
2023-07-18 17:05:00 -0300

    upload docs

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+ Pedido p = new Pedido(); //instancio um Pedido
+ // Pedido contem Iten(s), por isso em Pedido eu adiciono itens.
+ // COMPOSICAO tem a relacao de TEM UM  
+    }
+}
---
+++ src/Pedido.java
@@ -0,0 +1,10 @@
+public class Pedido {
+    private Itens i; 
+    // declarei uma variavel do tipo ITENS
+
+    public Pedido(){
+        // inicializar o objeto PEDIDO
+        i = new Itens();
+        // dentro do objeto Pedido eu instancio/ crio o objeto Itens
+    }
+}
